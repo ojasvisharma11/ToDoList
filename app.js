f9708adf2371219a3dba654cd28c1da24f014720
Ojasvi Sharma <ojasvi.sharma@intimtetec.com>
2021-02-02 08:32:55 +0530

    modified

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const todoInput = document.querySelector('.todo-input')
+// const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
 const filterOption = document.querySelector('.filter-todo')
